17167f473154a9208b0d3dbca41954a7bcec69e2
Vakada Jeevan <25l35a4321@vignaniit.edu.in>
2026-06-25 23:41:06 +0530

    Update single-number.cpp

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums ) {
+    int singleNumber(vector<int>& nums) {
         int result = 0;
         int n = nums.size();
         for(int i=0; i<n; i++){
