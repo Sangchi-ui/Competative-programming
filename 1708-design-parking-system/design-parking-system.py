21528a93946a4528546c692086636a70bb0fd591
Vakada Jeevan <25l35a4321@vignaniit.edu.in>
2026-09-24 15:15:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (21.18%) - LeetSync

--- 1708-design-parking-system/design-parking-system.py
+++ 1708-design-parking-system/design-parking-system.py
@@ -1,27 +1,10 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.big_cars = big
-        self.medium_cars = medium
-        self.small_cars = small
+        self._slots = [0, big, medium, small]
 
     def addCar(self, carType: int) -> bool:
-        if carType == 1:
-            if self.big_cars > 0:
-                self.big_cars -= 1
-                return True
-            else:
-                return False
-        if carType == 2:
-            if self.medium_cars > 0:
-                self.medium_cars -= 1
-                return True
-            else:
-                return False
-        if carType == 3:
-            if self.small_cars > 0:
-                self.small_cars -= 1
-                return True
-            else:
-                return False
+        if self._slots[carType] > 0:
+            self._slots[carType] -= 1
+            return True
         return False
